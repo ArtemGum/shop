c06d9a08f5e393799a08299b280fdec38e50478f
ArtemGum <artemgumarov840@gmail.com>
2019-05-09 17:34:42 +0500

    first commit

---
+++ src/pages/Product.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="wrapper-content wrapper-content--fixed">
+    <section>
+      <div class="container">
+        <h1></h1>
+      </div>
+    </section>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {};
+  },
+  created() {
+    // console.log({ route: this.$route, id: this.$route.params.id });
+    let id = this.$route.params.id;
+    let product = this.getters.getProduct(id);
+    console.log(product);
+  }
+};
+</script>
